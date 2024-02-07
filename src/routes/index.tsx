4ad799cdd6414a13a606d1170cd884bb2edf205a
rafaeldellaquila <rafael@dellaquila.dev>
2024-02-07 01:01:19 -0300

    :wrench: config router

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,16 @@
-console.log('routes')
+import { createBrowserRouter } from "react-router-dom";
+
+import Home from "@/pages/Home";
+import Profile from "@/pages/Profile/App";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },  {
+    path: "/profile",
+    element: <Profile />,
+  },
+]);
+
+export default router
