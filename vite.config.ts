6d9fd24dbc0a8adc4c747dbecf8cb297fbbf7b6a
rafaeldellaquila <rafael@dellaquila.dev>
2024-02-07 00:55:59 -0300

    :wrench: update configurations files :heavy_plus_sign: add dependencies for project development

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,21 @@
 import { resolve } from 'path'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
 
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': resolve(__dirname, '.'),
-      components: resolve(__dirname, './src/components/'),
-      public: resolve(__dirname, './public/'),
-      pages: resolve(__dirname, './src/pages/'),
-      types: resolve(__dirname, './src/@types/'),
-      styles: resolve(__dirname, './src/styles/'),
-    },
-  },
+      '@': resolve(__dirname, './src'),
+      '@components': resolve(__dirname, './src/components'),
+      '@assets': resolve(__dirname, './src/assets'),
+      '@hooks': resolve(__dirname, './src/hooks'),
+      '@i18n': resolve(__dirname, './src/i18n'),
+      '@pages': resolve(__dirname, './src/pages'),
+      '@redux': resolve(__dirname, './src/redux'),
+      '@theme': resolve(__dirname, './src/theme')
+    }
+  }
 })
