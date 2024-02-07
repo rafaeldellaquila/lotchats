6afd834743af958d2be0ab69000db1b1b771d6c4
rafaeldellaquila <rafael@dellaquila.dev>
2024-02-07 20:56:33 -0300

    :coffin: remove dead code

--- src/components/AvatarList/index.tsx
+++
@@ -1,7 +0,0 @@
-// import { Avatar } from '@mantine/core';
-
-const AvatarList: React.FC = () => {
-  return <div className='avatar-list'>{/* Mapeie e exiba avatares aqui */}</div>
-}
-
-export default AvatarList
