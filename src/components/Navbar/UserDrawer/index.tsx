5cec54763dc5e35954c156a4e5c20d15b88edf09
rafaeldellaquila <rafael@dellaquila.dev>
2024-02-08 02:36:02 -0300

    :lipstick: create UserDrawer

--- src/components/Navbar/UserDrawer/index.tsx
+++ src/components/Navbar/UserDrawer/index.tsx
@@ -8,7 +8,10 @@ import {
   ListItem,
   ListItemIcon,
   ListItemText,
-  Divider
+  Divider,
+  Avatar,
+  Typography,
+  Box
 } from '@mui/material'
 import { useTranslation } from 'react-i18next'
 
@@ -19,7 +22,7 @@ import StatusSelect from '@/components/shared/StatusSelect'
 const menuItems = [
   { icon: <GroupAddIcon />, text: 'create_group' },
   { icon: <LanguageIcon />, text: 'config' },
-  { icon: <MeetingRoomIcon />, text: 'quit' }
+  { icon: <MeetingRoomIcon color='error' />, text: 'quit' }
 ]
 
 const UserDrawer: React.FC<IDrawer> = ({ isOpen, toggle }) => {
@@ -27,17 +30,36 @@ const UserDrawer: React.FC<IDrawer> = ({ isOpen, toggle }) => {
 
   return (
     <Drawer isOpen={isOpen} toggle={toggle} anchor='right'>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'flex-start',
+          alignItems: 'center',
+          p: '1rem'
+        }}
+      >
+        <Avatar sx={{ mr: '1rem' }}>H</Avatar>
+        <Typography variant='body1' fontWeight='600'>
+          John Doe
+        </Typography>
+      </Box>
       <StatusSelect />
       <Divider sx={{ m: '.5rem', borderColor: 'transparent' }} />
-      <List>
+      <List sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
         {menuItems.map((item, index) => (
-          <ListItem key={index}>
+          <ListItem
+            key={index}
+            sx={{
+              width: 'auto',
+              mt: index === menuItems.length - 1 ? 'auto' : 0
+            }}
+          >
             <ListItemIcon>{item.icon}</ListItemIcon>
             <ListItemText
               primary={t(item.text)}
               primaryTypographyProps={{
                 variant: 'body1',
-                color: 'grey.100',
+                color: index === menuItems.length - 1 ? 'error' : 'grey.100',
                 fontWeight: 600,
                 style: {
                   whiteSpace: 'nowrap',
