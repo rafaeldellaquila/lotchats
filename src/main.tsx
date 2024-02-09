999d4b9876305e116670ec4b2d1b8868bb4771db
rafaeldellaquila <rafael@dellaquila.dev>
2024-02-09 17:35:47 -0300

    :lipstick: new main with persitor component and new router component

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,15 @@
-import { Global } from '@emotion/react'
-import { ThemeProvider } from '@mui/material'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { Provider } from 'react-redux'
-import { RouterProvider } from 'react-router-dom'
-import { PersistGate } from 'redux-persist/integration/react'
-
 import './i18n/index'
 
-import { persistor, store } from './redux/store'
-import router from './routes'
-import theme from './theme'
-import { GlobalStyles } from './theme/global'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+
+import Persistor from './layout/Persistor'
+import RouterComponent from './routes'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <ThemeProvider theme={theme}>
-          <Global styles={GlobalStyles} />
-          <RouterProvider router={router} />
-        </ThemeProvider>
-      </PersistGate>
-    </Provider>
-  </StrictMode>
+  <Persistor>
+    <BrowserRouter>
+      <RouterComponent />
+    </BrowserRouter>
+  </Persistor>
 )
